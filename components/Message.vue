0210ab413ef814f110af5a9258f44c1c64916095
Scott Jones <scottdj92@gmail.com>
2019-03-17 19:25:05 -0400

    Initial scaffold + component structure

---
+++ components/Message.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>{{ salutation }} {{ name }}, {{ message }}</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+type Data = {
+    message: string;
+}
+
+type Props = "name" | "salutation";
+export default Vue.component<Data, {}, {}, Props>("Message", {
+    data() {
+        return {
+            message: "Nuxt.js"
+        }
+    },
+    props: ["name", "salutation"]
+})
+</script>
+
+<style>
+    h1 {
+        color: coral;
+    }
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <Message name="Scott" salutation="Hello"></Message>
+</template>
+
+<script lang="ts">
+import Message from "../components/Message.vue";
+export default {
+    components: {
+        Message,
+    }
+}
+</script>
+
